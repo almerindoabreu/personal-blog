731eaca3f0c9e5da25da19e970701efc0766abdc
Almerindo Abreu <almerindo.neto@gmail.com>
2020-07-19 16:44:42 -0300

    static query for experience skills

--- src/components/SkillsExperience/index.js
+++ src/components/SkillsExperience/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react"
 
-import skills from "./content"
+import { useStaticQuery, graphql } from "gatsby"
+
+
 import ModalCalculoSkills from "../ModalCalculoSkills"
 import ModalExtratoSkill from "../ModalExtratoSkill"
 import Modal from "../Modal"
@@ -8,16 +10,128 @@ import Modal from "../Modal"
 import * as S from "./styled"
 
 const SkillsExperience = () => {
+  
   const [showSkills, setShowSkills] = useState(true)
   const [modalCalculoSkills, setModalCalculoSkills] = useState(false)
-  
+  const [maxExperienceSkills, setMaxExperienceSkills] = useState(0);
+  const [countLineSkills, setCountLineSkills] = useState(12);
+  const [skills, setSkills] = useState([]);
+
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark(filter: {fileAbsolutePath: {regex: "(posts|projects)/.*.md$/"}}) {
+        edges {
+          node {
+            id
+            frontmatter {
+              keyWords
+              category
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const contents = data.allMarkdownRemark.edges;
+ 
   useEffect(() => {
     const verifySizeOfScreen = () => {
       if (window.innerWidth <= 550) setShowSkills(false)
+      if (window.innerHeight <= 660) setCountLineSkills(6);
     }
-
+    getSkills(contents);
+    
     verifySizeOfScreen()
   }, [])
+
+  const getSkills = (contents) => {
+    let skills = [];
+
+    contents.map((
+      {
+        node: {
+          frontmatter: {
+            keyWords,
+            category,
+          },
+        },
+      },
+      i) => {
+        let keys = keyWords.split(', ');
+        keys.map(key => {
+          let skill = {key: key, category: category};
+          skills.push(skill);
+        })
+      })
+
+      const skillsFormatter = calculateSkills(skills);
+      getMaxExperience(skillsFormatter);
+      setSkills(skillsFormatter);
+  }
+
+  const calculateSkills = (skills) => {
+    const keys = getUnicKeys(skills);
+    let skillsFormatter = [];
+    keys.map(key => {
+      let count = 0;
+      skills.map(skill => {
+        if(key.value == skill.key){
+          count = count + calculateWeightCategory(skill.category);
+        }
+      })
+      skillsFormatter.push({name: key.value, experience: count})
+    })
+
+
+    // sort by experience
+    skillsFormatter.sort(function (a, b) {
+      return b.experience - a.experience;
+    });
+    
+    return skillsFormatter;
+  }
+
+  const calculateWeightCategory = (category) => {
+    switch (category) {
+      case "Artigo":
+        return 25;
+      case "CheatSheet":
+        return 20;
+      case "Projeto":
+        return 50;
+      default:
+        return 0;
+    }
+  }
+
+  const getMaxExperience = (skills) => {
+    let max = 0;
+    skills.map(skill => {
+      if(skill.experience > max){
+        max = skill.experience;
+      }
+    })
+
+    setMaxExperienceSkills(max);
+  }
+
+  const getUnicKeys = (skills) => {
+    let keys = [];
+
+    skills.map(skill => {
+      let flag = 1;
+      keys.map(key => {
+        if(key.value == skill.key)
+          flag = -1;
+      })
+      if(flag == 1){
+        keys.push({value: skill.key});
+      }
+    })
+
+    return keys;
+  }
 
   const handleShowSkills = () => {
     setShowSkills(!showSkills)
@@ -45,17 +159,22 @@ const SkillsExperience = () => {
         </S.HelpSkillsCountLink>
       </S.TitleWrapper>
       <S.WrapperSkillsItens show={showSkills == true ? "visible" : "hidden"}>
-        {skills.map((skill, i) => (
-          <S.SkillItem key={i}>
-            <S.SkillsLabelWrapper>
-              <S.SkillLabel>{skill.name}</S.SkillLabel>
-              <S.SkillLabel>{skill.experience + " exp."} </S.SkillLabel>
-            </S.SkillsLabelWrapper>
-            <S.SkillBar
-              experiencePorcent={skill.experiencePorcent}
-            ></S.SkillBar>
-          </S.SkillItem>
-        ))}
+        {skills.map((skill, i) => {
+          if(countLineSkills > i){
+            return (
+              <S.SkillItem key={i}>
+                <S.SkillsLabelWrapper>
+                  <S.SkillLabel>{skill.name}</S.SkillLabel>
+                  <S.SkillLabel>{skill.experience + " exp."} </S.SkillLabel>
+                </S.SkillsLabelWrapper>
+                <S.SkillBar
+                  experiencePorcent={(skill.experience / maxExperienceSkills) * 100}
+                ></S.SkillBar>
+              </S.SkillItem>
+            )
+          }
+        }
+        )}
       </S.WrapperSkillsItens>
       <S.MoreSkills show={showSkills == true ? "visible" : "hidden"}>
         <S.MoreSkillsLink href="/moreSkills" alt="Mais skills">
@@ -69,5 +188,7 @@ const SkillsExperience = () => {
     </>
   )
 }
+
+
 
 export default SkillsExperience
