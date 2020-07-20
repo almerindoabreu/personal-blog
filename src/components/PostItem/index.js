b7bc55dea3252ad15c11261b22cb0c737949c0b6
Almerindo Abreu <almerindo.neto@gmail.com>
2020-07-19 22:39:32 -0300

    improvement styled postItem

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import * as S from "./styled"
 
+import { TimeFive } from "@styled-icons/boxicons-solid/TimeFive"
+
 const PostItem = props => {
   const postList = props.postList
 
@@ -25,25 +27,33 @@ const PostItem = props => {
           i
         ) => {
           return (
-            <S.PostItem key={i}>
-              <S.PostLink to={slug}>
-                <S.PostItemDivHeader>
-                  <S.PostItemMainImage src={image} />
-                  <S.PostItemTitle>{title}</S.PostItemTitle>
-                </S.PostItemDivHeader>
-                <S.PostItemDivSubTitle>
-                  <S.PostItemSubTitle>{description}</S.PostItemSubTitle>
-                </S.PostItemDivSubTitle>
-                <S.PostItemDivFooter>
-                  <S.PostItemlabelFooter>
-                    {category + " • " + keyWords}
-                  </S.PostItemlabelFooter>
-                  <S.PostItemlabelFooter>
-                    {date + " • " + timeToRead + " min de leitura"}
-                  </S.PostItemlabelFooter>
-                </S.PostItemDivFooter>
-              </S.PostLink>
-            </S.PostItem>
+              <S.PostItem key={i}>
+                <S.PostLink to={slug}>
+                  <S.PostItemWrapperGroup>
+                    <S.PostItemGroup>
+                    <S.PostItemGroup>
+                          <S.PostItemTitle>{title}</S.PostItemTitle>
+                      </S.PostItemGroup>
+                      <S.PostItemGroup>
+                        <S.PostItemlabelFooter>
+                          <S.PostItemIconPurchaseTag />{category + " • " + keyWords}
+                        </S.PostItemlabelFooter>
+                        <S.PostItemlabelFooter>
+                          <S.PostItemIconTimeFive /> {date + " • " + timeToRead + " min de leitura"}
+                        </S.PostItemlabelFooter>
+                      </S.PostItemGroup>
+                      <S.PostItemImage>
+                          <S.PostItemMainImage src={image} />
+                      </S.PostItemImage>
+
+                      <S.PostItemGroup>
+                          <S.PostItemSubTitle>{description}</S.PostItemSubTitle>
+                      </S.PostItemGroup>
+
+                    </S.PostItemGroup>
+                  </S.PostItemWrapperGroup>
+                </S.PostLink>
+              </S.PostItem>
           )
         }
       )}
