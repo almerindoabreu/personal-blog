b3b4656dd20402baef997dce3eafefe3fbe1c5ce
Almerindo Abreu <almerindo.neto@gmail.com>
2021-04-07 23:09:50 -0300

    hotfix build error

--- src/utils/effectHover.js
+++ src/utils/effectHover.js
@@ -1,25 +1,25 @@
-const effectHover = () => {
+function effectHover() {
   const elements = document.querySelectorAll(".hover_effect");
   const elementsSpan = [];
-  
+
   elements.forEach((element, index) => {
-  
+
     // If The span element for this element does not exist in the array, add it.
     if (!elementsSpan[index])
       elementsSpan[index] = element.querySelector("span");
-  
+
     element.addEventListener("mouseover", e => {
       elementsSpan[index].style.left = e.pageX - element.offsetLeft + "px";
       elementsSpan[index].style.top = e.pageY - element.offsetTop + "px";
-  
+
     });
-  
+
     element.addEventListener("mouseout", e => {
       elementsSpan[index].style.left = e.pageX - element.offsetLeft + "px";
       elementsSpan[index].style.top = e.pageY - element.offsetTop + "px";
     });
   });
-  
+
 }
 
 export default effectHover;
